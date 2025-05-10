9db986c39c5d38814c74eb7acc725f8ad91b1e7d
lixd <946951925@qq.com>
2025-05-10 21:38:04 +0800

    全局配置解析器

--- blog/settings.py
+++ blog/settings.py
@@ -216,6 +216,11 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'apps.user.authentication.CustomJWTAuthentication',
     ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'EXCEPTION_HANDLER': 'blog.exception_handler.custom_exception_handler',
